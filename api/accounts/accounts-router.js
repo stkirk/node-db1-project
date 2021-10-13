88fc51449bfb9df5a02e350457a164b00826750d
stkirk <shaanekirk@gmail.com>
2021-10-13 14:12:35 -0400

    server wired to accounts router, catchall endpoint built and error handling endpoint built

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,29 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   // DO YOUR MAGIC
-})
+  res.send("hello from accounts");
+});
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
-
-router.put('/:id', (req, res, next) => {
+router.get("/:id", (req, res, next) => {
   // DO YOUR MAGIC
 });
 
-router.delete('/:id', (req, res, next) => {
+router.post("/", (req, res, next) => {
   // DO YOUR MAGIC
-})
+});
 
-router.use((err, req, res, next) => { // eslint-disable-line
+router.put("/:id", (req, res, next) => {
   // DO YOUR MAGIC
-})
+});
+
+router.delete("/:id", (req, res, next) => {
+  // DO YOUR MAGIC
+});
+
+router.use((err, req, res, next) => {
+  // eslint-disable-line
+  // DO YOUR MAGIC
+});
 
 module.exports = router;
